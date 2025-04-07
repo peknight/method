a29191937493c7961e2c2cfa047631b93e21c233
peknight <JKpeknight@gmail.com>
2025-04-07 11:32:58 +0800

    update

--- method-core/shared/src/main/scala/com/peknight/method/cascade/Source.scala
+++ method-core/shared/src/main/scala/com/peknight/method/cascade/Source.scala
@@ -9,19 +9,19 @@ import com.peknight.error.Error
 case class Source[F[_], E, A](
                                read: F[Either[E, Option[A]]],
                                write: A => F[Either[E, Unit]],
-                               continueOnError: Error => Boolean = _ => true,
+                               continueOnError: Error => Boolean = _ => false,
                                writeOnError: Error => Boolean = _ => true,
                              )
 object Source:
   def read[F[_]: Applicative, E, A](
                                      r: F[Either[E, Option[A]]],
-                                     continueOnError: Error => Boolean = _ => true,
+                                     continueOnError: Error => Boolean = _ => false,
                                      writeOnError: Error => Boolean = _ => true
                                    ): Source[F, E, A] =
     Source(r, _ => ().asRight[E].pure[F], continueOnError, writeOnError)
   def write[F[_]: Applicative, E, A](
                                      w: A => F[Either[E, Unit]],
-                                     continueOnError: Error => Boolean = _ => true,
+                                     continueOnError: Error => Boolean = _ => false,
                                      writeOnError: Error => Boolean = _ => true
                                    ): Source[F, E, A] =
     Source(none[A].asRight[E].pure[F], w, continueOnError, writeOnError)
